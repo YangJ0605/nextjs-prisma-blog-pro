9d4410e248c38bb3d7c4b437a1e73191532d5b10
unknown <1442122744@qq.com>
2022-05-14 11:31:18 +0800

    feat: 抽离global.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,8 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-import { PrismaClient } from '@prisma/client'
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-declare global {
-  var prisma: PrismaClient | undefined
-}
