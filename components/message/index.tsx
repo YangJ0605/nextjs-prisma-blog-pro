8b03987c77db4c5f1dac45cdd3438d71e0227d37
cc <1442122744@qq.com>
2022-05-30 18:07:31 +0800

    feat: message组件完善中...

--- components/message/index.tsx
+++ components/message/index.tsx
@@ -1,3 +1,41 @@
-function initModalContainer() {}
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function initModalContainer() {
+  let ele = document.getElementById('message-container')
+  if (!ele) {
+    let container = document.createElement('div')
+    container.id = 'message-container'
+    document.body.appendChild(container)
+    ele = document.getElementById('message-container')
+
+    ReactDOM.render(<MessageContainer />, ele)
+  }
+}
+initModalContainer()
+
+class MessageContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nodeList: [],
+      activeIDList: []
+    }
+  }
+  render() {
+    return (
+      <div className='w-screen fixed z-5000 left-0 top-0 flex flex-col'>
+        {this.state.nodeList.map((node, index) => (
+          <MessageItem
+            key={node.id}
+            config={node.config}
+            show={this.state.activeIDList.indexOf(node.id) > -1}
+          ></MessageItem>
+        ))}
+      </div>
+    )
+  }
+}
+function MessageItem(props) {}
 
 export default {}
